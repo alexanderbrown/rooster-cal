040f12e5d5be806363a24e8a03a3de865dee7a54
Alex Brown <alexpybrown@gmail.com>
2023-04-16 15:47:22 +0100

    Button spacing tweaked

--- src/components/ExportCalendar.tsx
+++ src/components/ExportCalendar.tsx
@@ -12,7 +12,7 @@ export default function ExportCalendar({calendar_id}: {calendar_id: string}){
 
     return(
         <div className="bg-white shadow-md rounded px-2 sm:px-8 py-4 my-4 flex flex-wrap">
-            <div className='mx-2 border rounded shadow-md hover:shadow-lg grow cursor-pointer bg-gray-50 group flex justify-between'>
+            <div className='m-2 border rounded shadow-md hover:shadow-lg grow cursor-pointer bg-gray-50 group flex justify-between'>
                 <a className={`p-4 h-12 sm:h-14 text-sm sm:text-base group-hover:bg-white
                                 flex items-center grow justify-center`}
                     href={`${window.location.protocol}//${calendar_url}`}>
@@ -28,7 +28,7 @@ export default function ExportCalendar({calendar_id}: {calendar_id: string}){
                     {copyButtonTextOptions[copyButtonState]} 
                 </a>
             </div>
-            <a className='bg-gray-50 border shadow-md hover:shadow-lg p-4 h-12 sm:h-14 mx-2 grow rounded hover:bg-white cursor-pointer text-sm sm:text-base flex items-center justify-center'
+            <a className='bg-gray-50 border shadow-md hover:shadow-lg p-4 h-12 sm:h-14 m-2 grow rounded hover:bg-white cursor-pointer text-sm sm:text-base flex items-center justify-center'
                 href={`https://calendar.google.com/?cid=webcal://${calendar_url}`} target='_blank'>
                 <div className='w-max'>
                     {google} Add To Google
