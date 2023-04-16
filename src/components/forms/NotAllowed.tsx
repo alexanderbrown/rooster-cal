bc99b7609b0310e98b347c9e22fd5438363df67c
Alex Brown <alexpybrown@gmail.com>
2023-04-16 16:02:45 +0100

    Not allowed page updated for responsive design

--- src/components/forms/NotAllowed.tsx
+++ src/components/forms/NotAllowed.tsx
@@ -1,9 +1,9 @@
 export default function NotAllowed(){
     return (
-        <div className="flex flex-col w-full items-center">
-            <div className="mt-20 flex flex-col items-center text-lg text-slate-800">
+        <div className="flex flex-col w-full items-center px-4">
+            <div className="mt-20 flex flex-col items-center sm:text-lg text-slate-800">
             <p>Unfortunately, Rooster is currently in Beta, and only available for selected users</p>
-            <p className="text-base mt-2">Please contact Alex if you would like an invite</p>
+            <p className="text-sm sm:text-base mt-2">Please contact Alex if you would like an invite</p>
             </div>
         </div>
 
